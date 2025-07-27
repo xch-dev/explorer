a107b603bc5a1cef04983bff201ec8e9b5f926c6
Rigidity <me@rigidnetwork.com>
2025-07-27 15:27:21 -0400

    Rename

--- crates/types/src/lib.rs
+++ crates/types/src/lib.rs
@@ -1,9 +1,9 @@
 mod coin_record;
-mod coin_spend_record;
+mod coin_spend;
 mod coin_type;
 mod p2_puzzle;
 
 pub use coin_record::*;
-pub use coin_spend_record::*;
+pub use coin_spend::*;
 pub use coin_type::*;
 pub use p2_puzzle::*;
