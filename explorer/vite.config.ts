dba1402a5d3f4be607318ec52a2a1990ea00aacc
Rigidity <me@rigidnetwork.com>
2025-07-27 20:06:19 -0400

    Scaffold UI

---
+++ explorer/vite.config.ts
@@ -0,0 +1,9 @@
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
+import topLevelAwait from "vite-plugin-top-level-await";
+import wasm from "vite-plugin-wasm";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), wasm(), topLevelAwait()],
+});
